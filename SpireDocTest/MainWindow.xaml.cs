85cf3dcf2f2d0cce8faecd606208515ff7b87d18
Ostap <ostap.moravskyi@gmail.com>
2017-11-19 19:11:32 +0200

    First commit

---
+++ SpireDocTest/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using FirstFloor.ModernUI.Windows.Controls;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Windows.Input;
+
+namespace SpireDocTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : ModernWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+    }
+}
